c2f113da58926044ce1e751376ed1bc8b692a412
iSenity1812 <92313201+iSenity1812@users.noreply.github.com>
2024-09-08 22:56:13 +0700

    Update IProductFactory.java

--- src/IProductFactory.java
+++ src/IProductFactory.java
@@ -1,3 +1,9 @@
+
+/**
+ *
+ * @author isepipi
+ */
+
 public interface IProductFactory {
     Product createProduct(String id, String name, String brandId, String categoryId, int modelYear, double price);
 }
